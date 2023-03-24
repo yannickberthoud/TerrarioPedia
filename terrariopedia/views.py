36a08816de813cc3bdeaf8f707525419c6f33f20
Yannick Berthoud <yannick.berthoud@outlook.com>
2023-03-24 10:22:32 +0100

    edit views

--- terrariopedia/views.py
+++ terrariopedia/views.py
@@ -3,5 +3,5 @@ from card.models import Card
 
 
 def home(request):
-    cards = Card.objects.order_by('genus', 'species')[:16]
+    cards = Card.objects.all()[:16]
     return render(request, 'terrariopedia/home.html', {'cards': cards})
